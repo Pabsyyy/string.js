948a46653846305256cc039046e25f21caa3e403
Pabsyyy <christiangalinato1218@gmail.com>
2024-09-15 10:06:46 +0800

    initial commit

---
+++ string.js
@@ -0,0 +1,23 @@
+let favActorFirstName = 'Tom';
+console.log("First Name:", favActorFirstName);
+
+let favActorLastName = 'Cruise';
+console.log("Last Name:", favActorLastName);
+
+//CONCATENATION
+let fullName = favActorFirstName + ' ' + favActorLastName;
+console.log("Full Name:", fullName);
+
+let uppercase = 'Tom Cruise';
+console.log("Uppercase Name:", uppercase.toLowerCase());
+
+let lowercase = 'Tom Cruise'; 
+console.log("Lowercase Name:", lowercase.toUpperCase());
+
+let lowerCase = 'NUGGETS';
+console.log("My favorite pet:", lowerCase);
+
+//APPEND
+let message = `My favorite actor is TOM CRUISE`;
+message += " his best show is Top Gun 2.";
+console.log(message);
